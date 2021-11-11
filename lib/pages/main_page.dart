a9c3e8ba0a38bdf401ddbd53d12ccb69b1978c25
Dmitry <dmitry.maslov.almeza@gmail.com>
2021-11-11 10:34:46 +0300

    Домашнее задание №2 - кнопка ActionButton

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:superheroes/blocs/main_bloc.dart';
 import 'package:superheroes/resources/superheroes_colors.dart';
+import 'package:superheroes/widgets/action_button.dart';
 
 class MainPage extends StatefulWidget {
   MainPage({Key? key}) : super(key: key);
@@ -42,15 +43,9 @@ class MainPageContent extends StatelessWidget {
         MainPageStateWidget(),
         Align(
           alignment: Alignment.bottomCenter,
-          child: GestureDetector(
+          child: ActionButton(
             onTap: () => bloc.nextState(),
-            child: Text(
-              "Next state".toUpperCase(),
-              style: TextStyle(
-                fontSize: 20,
-                color: Colors.white,
-              ),
-            ),
+            text: "Next state",
           ),
         ),
       ],
--- lib/widgets/action_button.dart
+++ lib/widgets/action_button.dart
@@ -1,10 +1,34 @@
 import 'package:flutter/material.dart';
 
 class ActionButton extends StatelessWidget {
-  const ActionButton({Key? key}) : super(key: key);
+  final String text;
+  final VoidCallback onTap;
+
+  const ActionButton({
+    Key? key,
+    required this.text,
+    required this.onTap,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        padding: EdgeInsets.symmetric(vertical: 8, horizontal: 20),
+        decoration: BoxDecoration(
+          color: Color(0xFF00BCD4),
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: Text(
+          text.toUpperCase(),
+          style: TextStyle(
+            fontWeight: FontWeight.w700,
+            fontSize: 14,
+            color: Colors.white,
+          ),
+        ),
+      ),
+    );
   }
 }
