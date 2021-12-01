06a93b2fb5cf2bffb0a7228d7f10d285a31818c1
Dmitry <dmitry.maslov.almeza@gmail.com>
2021-12-01 14:50:54 +0300

    Урок 4, 1:55:27

--- lib/model/powerstats.dart
+++ lib/model/powerstats.dart
@@ -20,6 +20,32 @@ class Powerstats {
     required this.combat,
   });
 
+  bool isNotNull() =>
+      intelligence != 'null' &&
+      strength != 'null' &&
+      speed != 'null' &&
+      durability != 'null' &&
+      power != 'null' &&
+      combat != 'null';
+
+  double get intelligencePercent => _convertStringToPercent(intelligence);
+
+  double get strengthPercent => _convertStringToPercent(strength);
+
+  double get speedPercent => _convertStringToPercent(speed);
+
+  double get durabilityPercent => _convertStringToPercent(durability);
+
+  double get powerPercent => _convertStringToPercent(power);
+
+  double get combatPercent => _convertStringToPercent(combat);
+
+  double _convertStringToPercent(final String value) {
+    final intValue = int.tryParse(value);
+    if (intValue == null) return 0;
+    return intValue / 100;
+  }
+
   factory Powerstats.fromJson(final Map<String, dynamic> json) =>
       _$PowerstatsFromJson(json);
 
--- lib/pages/superhero_page.dart
+++ lib/pages/superhero_page.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:superheroes/model/biography.dart';
@@ -43,7 +45,20 @@ class SuperheroPage extends StatelessWidget {
       body: CustomScrollView(
         slivers: [
           SuperheroAppBar(superhero: superhero),
-          SliverToBoxAdapter(),
+          SliverToBoxAdapter(
+            child: Column(
+              children: [
+                const SizedBox(height: 30),
+                if (superhero.powerstats.isNotNull())
+                  PowerstatsWidget(
+                    powerstats: superhero.powerstats,
+                  ),
+                BiographyWidget(
+                  biography: superhero.biography,
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
@@ -79,6 +94,234 @@ class SuperheroAppBar extends StatelessWidget {
         background: CachedNetworkImage(
           imageUrl: superhero.image.url,
           fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+}
+
+class PowerstatsWidget extends StatelessWidget {
+  final Powerstats powerstats;
+
+  const PowerstatsWidget({
+    Key? key,
+    required this.powerstats,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Center(
+          child: Text(
+            "Powerstats".toUpperCase(),
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 18,
+              letterSpacing: 0.4,
+              fontWeight: FontWeight.w700,
+            ),
+          ),
+        ),
+        const SizedBox(height: 24),
+        Row(
+          children: [
+            const SizedBox(width: 16),
+            Expanded(
+              child: Center(
+                child: PowerstatWidget(
+                  name: 'Intelligence',
+                  value: powerstats.intelligencePercent,
+                ),
+              ),
+            ),
+            Expanded(
+              child: Center(
+                child: PowerstatWidget(
+                  name: 'Strength',
+                  value: powerstats.strengthPercent,
+                ),
+              ),
+            ),
+            Expanded(
+              child: Center(
+                child: PowerstatWidget(
+                  name: 'Speed',
+                  value: powerstats.strengthPercent,
+                ),
+              ),
+            ),
+            const SizedBox(width: 16),
+          ],
+        ),
+        const SizedBox(height: 20),
+        Row(
+          children: [
+            const SizedBox(width: 16),
+            Expanded(
+              child: Center(
+                child: PowerstatWidget(
+                  name: 'Durability',
+                  value: powerstats.durabilityPercent,
+                ),
+              ),
+            ),
+            Expanded(
+              child: Center(
+                child: PowerstatWidget(
+                  name: 'Power',
+                  value: powerstats.powerPercent,
+                ),
+              ),
+            ),
+            Expanded(
+              child: Center(
+                child: PowerstatWidget(
+                  name: 'Combat',
+                  value: powerstats.combatPercent,
+                ),
+              ),
+            ),
+            const SizedBox(width: 16),
+          ],
+        ),
+        const SizedBox(height: 36),
+      ],
+    );
+  }
+}
+
+class PowerstatWidget extends StatelessWidget {
+  final String name;
+  final double value;
+
+  const PowerstatWidget({
+    Key? key,
+    required this.name,
+    required this.value,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      alignment: Alignment.topCenter,
+      children: [
+        ArcWidget(value: value, color: calculateColorByValue()),
+        Padding(
+          padding: const EdgeInsets.only(top: 17),
+          child: Text(
+            "${(value * 100).toInt()}",
+            style: TextStyle(
+              color: calculateColorByValue(),
+              fontWeight: FontWeight.w700,
+              fontSize: 18,
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 44),
+          child: Text(
+            name.toUpperCase(),
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.w700,
+              fontSize: 12,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Color calculateColorByValue() {
+    if (value <= 0.5) {
+      return Color.lerp(
+        Colors.red,
+        Colors.orangeAccent,
+        value / 0.5,
+      )!;
+    } else {
+      return Color.lerp(
+        Colors.orangeAccent,
+        Colors.green,
+        (value - 0.5) / 0.5,
+      )!;
+    }
+  }
+}
+
+class ArcWidget extends StatelessWidget {
+  final double value;
+  final Color color;
+
+  const ArcWidget({
+    Key? key,
+    required this.value,
+    required this.color,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomPaint(
+      painter: ArcCustomPainter(value: value, color: color),
+      size: Size(66, 33),
+    );
+  }
+}
+
+class ArcCustomPainter extends CustomPainter {
+  final double value;
+  final Color color;
+
+  ArcCustomPainter({
+    required this.value,
+    required this.color,
+  });
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final rect = Rect.fromLTWH(0, 0, size.width, size.height * 2);
+    final paint = Paint()
+      ..color = color
+      ..style = PaintingStyle.stroke
+      ..strokeCap = StrokeCap.round
+      ..strokeWidth = 6;
+    final backgroundPaint = Paint()
+      ..color = Colors.white24
+      ..style = PaintingStyle.stroke
+      ..strokeCap = StrokeCap.round
+      ..strokeWidth = 6;
+    canvas.drawArc(rect, pi, pi, false, backgroundPaint);
+    canvas.drawArc(rect, pi, pi * value, false, paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    if (oldDelegate is ArcCustomPainter) {
+      return oldDelegate.value != value || oldDelegate.color != color;
+    }
+    return true;
+  }
+}
+
+class BiographyWidget extends StatelessWidget {
+  final Biography biography;
+
+  const BiographyWidget({
+    Key? key,
+    required this.biography,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 300,
+      alignment: Alignment.center,
+      child: Text(
+        biography.toJson().toString(),
+        style: TextStyle(
+          color: Colors.white,
         ),
       ),
     );
